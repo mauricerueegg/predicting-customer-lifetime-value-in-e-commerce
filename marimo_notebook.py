3b457afe3844639a54e49263b1e31c6343ea1de9
Maurice Rueegg <maurice.rueegg@gmail.ch>
2026-05-21 08:34:20 +0200

    first commit

---
+++ marimo_notebook.py
@@ -0,0 +1,20 @@
+import marimo
+
+__generated_with = "0.23.6"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
